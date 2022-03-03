c416d8b9a3a4630d824d13cd601949aaa3a003c6
kmirella <keresztesmirella00@gmail.com>
2022-03-03 16:22:15 +0100

    const inputList

--- frontend/script.js
+++ frontend/script.js
@@ -1,41 +1,32 @@
-function Country(name, short, population, flag, continent) {
-this.name = name;
-this.short = short;
-this.population = population;
-this.flag = flag;
-this.continent = continent;
-}
+const form = () => {
+    return `
+    <form>
+        <div class="input1">
+            <input type="text" />
+        </div>
+        <div class="input2">
+            <input type="text" />
+        </div>
+        <div class="input3">
+            <input type="text" />
+        </div>
+        <button>Send</button>
+    </form>
+    `;
+};
 
-// components 
-const header = (logo) => {
-    return`
-    <header>
-        <a id="logo">${logo}</a>
-        <button></button>
-    </header>
-    `
-}
+function loadEvent() {
+console.log("load");
+const rootElement = document.getElementById("root")
 
+rootElement.insertAdjacentHTML("beforeend", form())
 
- const loadEvent = async _ => {  
-
-// get data
-    const countryRes = await fetch("https://restcountries.com/v3.1/all");
-    const countryArr = await countryRes.json(); // await: megvárja a letöltődést  // a countryArr várja meg míg a countryRes létrejön és lefut
-    // console.log(countryArr[0]);
-
-    // process data
-    let countries = countryArr.map(function(country) {
-        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0])
-
+const inputList = document.querySelectorAll("input")
+for (const input of inputList) {
+    input.addEventListener("input", function(e) {
+        console.log(e.target.value);
     })
-    console.log(countries);
-
-    const rootElement = document.getElementById("root");
-    rootElement.insertAdjacentHTML("beforeend", header("Countries"));
 }
-
-
-window.addEventListener("load", loadEvent) 
+}
 
 
